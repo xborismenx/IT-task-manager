1cf81a3fedeca678887574a9d35d0ab489841804
xboris <boris.tikhonov.21@gmail.com>
2024-05-24 00:41:13 +0300

    for model tasktype and position added __str__ method, for worker aded meta with verbose_name

--- task_manager/models.py
+++ task_manager/models.py
@@ -5,13 +5,26 @@ from django.db import models
 class Position(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Worker(AbstractUser):
     position = models.ForeignKey(Position, on_delete=models.SET_NULL, null=True, blank=True)
 
+    class Meta:
+        verbose_name = "Worker"
+        verbose_name_plural = "Workers"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.username}) - position {self.position}"
+
 
 class TaskType(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
