ed639ef1bd0ed2ba8d65ac847bc08ba9a498b65a
xboris <boris.tikhonov.21@gmail.com>
2024-05-23 19:20:14 +0300

    create model Position and  Worker

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.SET_NULL, null=True, blank=True)
+
