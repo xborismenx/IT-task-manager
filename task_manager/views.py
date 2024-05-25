fc1972f499f8dfc07540c61f850b3800e1e33dfc
xboris <boris.tikhonov.21@gmail.com>
2024-05-26 00:29:38 +0300

    created view index

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from task_manager.models import Task, Worker
+
+
+def index(request):
+    workers = Worker.objects.all().count()
+    tasks = Task.objects.all().count()
+    context = {
+        'workers': workers,
+        'tasks': tasks
+    }
+    return render(request, "task_manager/index.html", context=context)
