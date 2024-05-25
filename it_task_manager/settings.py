499efff9c871d9c8750505448043f48a09a6e0ac
xboris <boris.tikhonov.21@gmail.com>
2024-05-25 21:52:22 +0300

    configured django toolbar

--- it_task_manager/settings.py
+++ it_task_manager/settings.py
@@ -35,11 +35,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task_manager'
+
+    "debug_toolbar",
+
+    'task_manager',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,3 +123,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = "task_manager.Worker"
+
+INTERNAL_IPS = ["127.0.0.1", ]
