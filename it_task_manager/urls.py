4b859fa65bead270e0322053abda9f7a94540caa
xboris <boris.tikhonov.21@gmail.com>
2024-05-25 21:54:17 +0300

    added path for django_toolbar.urls

--- it_task_manager/urls.py
+++ it_task_manager/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("task_manager.urls", namespace="task_manager"))
-
+    path("", include("task_manager.urls", namespace="task_manager")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 
