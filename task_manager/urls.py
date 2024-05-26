a9dcc89c288d59c8c88b2d66bac35b895166c6ff
xboris <boris.tikhonov.21@gmail.com>
2024-05-27 00:06:08 +0300

    added path tasks/ & workers/

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from task_manager import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("tasks/", views.TaskListView.as_view(), name="task-list"),
+    path("workers/", views.WorkerListView.as_view(), name="worker-list"),
+
 ]
 
 app_name = "task_manager"
