f59c40e46001027808365ad9b02baaed5be7980d
xboris <boris.tikhonov.21@gmail.com>
2024-05-27 00:07:03 +0300

    create task and worker list views

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from task_manager.models import Task, Worker
 
@@ -11,3 +12,15 @@ def index(request):
         'tasks': tasks
     }
     return render(request, "task_manager/index.html", context=context)
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    template_name = "task_manager/task_list.html"
+    context_object_name = "task_list"
+
+
+class WorkerListView(generic.ListView):
+    model = Worker
+    template_name = "task_manager/worker_list.html"
+    context_object_name = "worker_list"
