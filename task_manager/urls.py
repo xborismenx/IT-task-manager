09b25a51db2a087d17a61f54abaa37bb8bcc596f
xboris <boris.tikhonov.21@gmail.com>
2024-05-28 00:13:23 +0300

    add url patterns for task create, detail, and update views

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -5,6 +5,9 @@ from task_manager import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("tasks/", views.TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", views.TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/", views.TaskDetailView.as_view(), name="task-detail"),
+    path("tasks/<int:pk>/update/", views.TaskUpdateView.as_view(), name="task-update"),
     path("workers/", views.WorkerListView.as_view(), name="worker-list"),
 
 ]
