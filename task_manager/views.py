43f59a06357287f989615a2e137587233c18d162
xboris <boris.tikhonov.21@gmail.com>
2024-05-27 23:08:08 +0300

    created TaskCreateView and TaskDetailView in views.py

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse, reverse_lazy
 from django.views import generic
 
 from task_manager.models import Task, Worker
@@ -18,6 +19,18 @@ class TaskListView(generic.ListView):
     model = Task
     template_name = "task_manager/task_list.html"
     context_object_name = "task_list"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:task-list")
+    template_name = "task_manager/task_form.html"
+
+
+class TaskDetailView(generic.DetailView):
+    model = Task
+    template_name = "task_manager/task_detail.html"
 
 
 class WorkerListView(generic.ListView):
