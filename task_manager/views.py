8d8961ffdd6de31e3b3885fa9c3c132a1edad4b6
xboris <boris.tikhonov.21@gmail.com>
2024-05-28 22:49:38 +0300

    created for task update, delete views and for worker create, update views

--- task_manager/views.py
+++ task_manager/views.py
@@ -18,6 +18,7 @@ def index(request):
 class TaskListView(generic.ListView):
     model = Task
     queryset = Task.objects.select_related('task_type').prefetch_related('assignees')
+    paginate_by = 2
     template_name = "task_manager/task_list.html"
     context_object_name = "task_list"
 
@@ -34,7 +35,34 @@ class TaskDetailView(generic.DetailView):
     template_name = "task_manager/task_detail.html"
 
 
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:task-list")
+    template_name = "task_manager/task_form.html"
+
+
+class DeleteTaskView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("task_manager:task-list")
+    template_name = "task_manager/task_confirm_delete.html"
+
+
 class WorkerListView(generic.ListView):
     model = Worker
     template_name = "task_manager/worker_list.html"
     context_object_name = "worker_list"
+
+
+class WorkerCreateView(generic.CreateView):
+    model = Worker
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:worker-list")
+    template_name = "task_manager/worker_form.html"
+
+
+class WorkerUpdateView(generic.UpdateView):
+    model = Worker
+    fields = "__all__"
+    success_url = reverse_lazy("task_manager:worker")
+    template_name = "task_manager/worker_form.html"
