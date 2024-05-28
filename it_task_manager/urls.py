6911e79da084fcb1d2a72b8d3c932f21a044686b
xboris <boris.tikhonov.21@gmail.com>
2024-05-28 22:51:10 +0300

    added path accounts/ for django.contrib.auth.urls

--- it_task_manager/urls.py
+++ it_task_manager/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("task_manager.urls", namespace="task_manager")),
     path("__debug__/", include("debug_toolbar.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
 
