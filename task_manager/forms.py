690a559901442a10a6fa9c0b3810de35cf4b863f
xboris <boris.tikhonov.21@gmail.com>
2024-06-01 01:26:37 +0300

    created search task form for search in task list

---
+++ task_manager/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+class SearchTaskForm(forms.Form):
+    name = forms.CharField(
+        max_length=100,
+        required=False,
+        label="",
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "search by name"
+            }
+        )
+    )
