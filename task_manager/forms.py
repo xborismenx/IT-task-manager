639616cf1c6be8b30e37302420cf07671a8b243c
xboris <boris.tikhonov.21@gmail.com>
2024-06-07 01:20:35 +0300

    added task form and search task form

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,4 +1,24 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+from task_manager.models import Task, Worker
+
+
+class TaskForm(forms.ModelForm):
+    deadline = forms.DateTimeField(widget=forms.DateTimeInput(
+        attrs={
+            "type": "datetime-local"
+        }
+    ))
+    assignees = forms.ModelMultipleChoiceField(
+        queryset=Worker.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    class Meta:
+        model = Task
+        fields = "__all__"
 
 
 class SearchTaskForm(forms.Form):
