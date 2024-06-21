dcbb1949e037649f1da95612c97e476020cf5d4c
xboris <boris.tikhonov.21@gmail.com>
2024-06-22 01:12:38 +0300

    deleted nonrequired imports

--- task_manager/tests/test_models.py
+++ task_manager/tests/test_models.py
@@ -4,8 +4,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from it_task_manager.settings import LOGIN_URL
-from task_manager.models import Position, TaskType, Task, Worker
+from task_manager.models import Position, TaskType, Task
 
 
 class TestModels(TestCase):
