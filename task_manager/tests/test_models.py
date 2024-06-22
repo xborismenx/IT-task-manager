06483197cb9376a4c9bc18bb6f9f46b7ab132825
xboris <boris.tikhonov.21@gmail.com>
2024-06-23 00:19:45 +0300

    deleted tests which test create, update delete.Added more specify for fields and methods model

--- task_manager/tests/test_models.py
+++ task_manager/tests/test_models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from django.contrib.auth import get_user_model
+from django.db.models.fields.related import ForeignKey
 from django.test import TestCase
 from django.urls import reverse
 
@@ -24,7 +25,7 @@ class TestModels(TestCase):
             first_name=self.first_name1,
             last_name=self.last_name1,
             position=self.position_developer,
-            )
+        )
         self.worker1.set_password(self.password1)
         self.worker1.is_active = True
         self.worker1.save()
@@ -55,89 +56,36 @@ class TestModels(TestCase):
         )
         self.task_1.assignees.set([self.worker1, self.worker2])
 
-    def test_model_position_name_field(self):
+    def test_position_name_field(self):
         position = Position.objects.get(name='developer')
         name = position._meta.get_field("name").verbose_name
         self.assertEqual(name, "name")
 
-    def test_model_task_type_field(self):
+    def test_task_type_field(self):
         task_type = TaskType.objects.get(id=1)
         name = task_type._meta.get_field("name").verbose_name
         self.assertEqual(name, "name")
 
-    def test_model_worker_position_field(self):
+    def test_worker_position_field(self):
+        position = get_user_model()._meta.get_field("position")
         self.assertEqual(self.worker1.position, self.position_developer)
+        self.assertTrue(isinstance(position, ForeignKey))
 
-    def test_create_worker(self):
-        username = "some_username"
-        first_name = "some_first_name"
-        last_name = "some_last_name"
-        password = "PASSWORD"
+    def test_worker_absolute_url(self):
+        self.assertEqual(self.worker1.get_absolute_url(), "/workers/1/update/")
 
-        worker = get_user_model().objects.create_user(username=username,
-                                                      first_name=first_name,
-                                                      last_name=last_name)
+    def test_worker_str(self):
+        self.assertEqual(self.worker1.__str__(), 'ivan bogunov (worker1) - position developer')
 
-        worker.set_password(password)
-        worker.save()
+    def test_task_model_fields(self):
+        self.assertEqual(self.task_1.name, self.task_name)
+        self.assertEqual(self.task_1.description, self.task_description)
+        self.assertEqual(self.task_1.deadline, self.task_deadline)
+        self.assertEqual(self.task_1.is_completed, self.task_is_completed)
+        self.assertEqual(self.task_1.priority, self.task_priority)
+        self.assertEqual(self.task_1.task_type, self.task_type)
+        self.assertEqual(self.task_1.assignees.count(), 2)
 
-        self.assertEqual(worker.username, username)
-        self.assertEqual(worker.first_name, first_name)
-        self.assertEqual(worker.last_name, last_name)
+    def test_absolute_url(self):
+        self.assertEqual(self.task_1.get_absolute_url(), "/tasks/1/")
 
-        self.assertTrue(worker.check_password(password))
-
-    def test_update_worker(self):
-        update_url = reverse('task_manager:worker-update', kwargs={'pk': self.worker1.id})
-        response = self.client.post(update_url, data={
-            "username": self.worker1.username,
-            "position": self.position_designer.id,
-            "password": self.password1,
-            "date_joined": self.worker1.date_joined
-        })
-
-        self.assertEqual(response.status_code, 302)
-
-    def test_create_task(self):
-        task_id = 2
-        task_name = "fixed footer buttons"
-        task_description = "fix footer buttons because..."
-        task_deadline = datetime.strptime("2025-01-25 23:00:00", "%Y-%m-%d %H:%M:%S")
-        task_is_completed = False
-        task_priority = "1"
-
-        task = Task.objects.create(
-            id=task_id,
-            name=task_name,
-            description=task_description,
-            deadline=task_deadline,
-            is_completed=task_is_completed,
-            priority=task_priority,
-            task_type=self.task_type,
-        )
-        task.assignees.set([self.worker1, self.worker2])
-        self.assertEqual(task.name, task_name)
-        self.assertEqual(task.description, task_description)
-        self.assertEqual(task.deadline, task_deadline)
-        self.assertEqual(task.priority, task_priority)
-        self.assertEqual(task.task_type, self.task_type)
-        self.assertEqual(list(task.assignees.get_queryset()), [self.worker1, self.worker2])
-
-    def test_update_task(self):
-        test_name = "test name task"
-        update_url = reverse("task_manager:task-update", kwargs={"pk": self.task_1.pk})
-        response = self.client.post(update_url,
-                                    {"name": test_name,
-                                     "priority":self.task_priority,
-                                     "description": self.task_description,
-                                     "deadline": self.task_deadline,
-                                     "assignees": [self.worker1.id, self.worker2.id]})
-
-        self.assertEqual(response.status_code, 302)
-
-    def test_delete_task(self):
-        print(self.worker1.is_authenticated)
-        delete_url = reverse("task_manager:task-delete", kwargs={"pk": self.task_1.id})
-        response = self.client.post(delete_url)
-        self.assertEqual(response.status_code, 302)
-        self.assertFalse(Task.objects.filter(id=self.task_1.id).exists())
