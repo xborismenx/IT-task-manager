2bf1a4469f031170e1e8bb49e846510c0cb676cb
xboris <boris.tikhonov.21@gmail.com>
2024-06-24 00:59:08 +0300

    used in settings.py variables from .env

--- it_task_manager/settings.py
+++ it_task_manager/settings.py
@@ -1,3 +1,5 @@
+import environ
+import os
 """
 Django settings for it_task_manager project.
 
@@ -17,12 +19,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v5)ms&dr(q*t5va7ei(*q9!dbj8b_^!=g1uaqr0*$yia#_2y2@'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = []
 
