446ba4c7daed2916d1d24ec3319480cbd6328b2f
xboris <boris.tikhonov.21@gmail.com>
2024-06-26 01:07:59 +0300

    added environ for secret_key & debug

--- it_task_manager/settings.py
+++ it_task_manager/settings.py
@@ -28,10 +28,10 @@ env = environ.Env(
 
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "") != "False"
+DEBUG = os.environ["DEBUG"]
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
